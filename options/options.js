e6694bf047c3d432a6545c4b4c6f6e56e1ee6b33
Crowdbotics <team@crowdbotics.com>
2021-09-09 08:14:11 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "ride_30413Identifier",
+  url: "https://ride_30413Identifier.botics.co",
+  api: "https://ride_30413Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
